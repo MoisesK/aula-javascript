af956e43ca9d6b5e21442f7dcf1730d60c75b542
Moisés Kalebe <72888174+MoisesK@users.noreply.github.com>
2020-10-21 11:34:06 -0300

    adicionado arquivos por upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+    
+    //alert("Obrigado por clicar");
+}
+
+function redirect(){
+    window.open("https://www.google.com");
+}
+
+
+alert("Bom dia, Bem vindo ao site de teste de HTML5,CSS, e javascript!")
+var d = new Date()
+
+alert(d);
+
+var idade = prompt("Antes de você logar, poderia me informar sua idade?");    
+if(idade >= 18){
+    alert("Tudo bem, você pode prosseguir!")
+}else{
+    alert("Volte quando estiver um pouco mais velho!")
+
+    };
+
+
+
+//lista
+
+//var lista = ["maça", "uva", "laranja"]
+//lista.push("uva");
+//lista.pop(); 
+//alert(list)
+//console.log(lista)
+
+/* Básico console e alert
+var nome = "Moisés Kalebe";
+var idade = "34"
+var idade2 = 26
+var idade3 = idade + idade2
+// alert("Bem-vindo " + nome);
+// alert(idade + " + " + idade2 + " = " + idade3)
+//alert(idade.replace("34"," Não tenho 34, tenho 20 anos"));
+
+console.log(nome)
+console.log(idade)
+*/
